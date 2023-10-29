34138e5a46032243b55ea444fac95f135fcbeb87
Sky Blaise <69448615+SkyBlaise99@users.noreply.github.com>
2023-10-29 17:29:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-
-# ./get-reposense.py --master
-java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+if [ "$1" = "true" ]; then
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A -st $2
+else
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+fi
