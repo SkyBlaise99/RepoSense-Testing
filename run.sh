335f5d4f902d94c36f6817f4c3facf63adfc0f92
SkyBlaise <zsc990124@gmail.com>
2024-02-05 14:56:42 +0800

    update sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 if [ "$1" = "true" ]; then
-    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A -dt $2
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A
+elif [ "$2" <> "0.51" ]; then
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A -ot $2
 else
     java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
 fi
