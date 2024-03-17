67f900a25fdf906251c87c22bd9ca92a09a07a4b
Sky Blaise <69448615+SkyBlaise99@users.noreply.github.com>
2024-03-17 19:45:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-if [ "$1" = "true" ]; then
+if [ "$1" = "true" ] && [ "$2" = "0.51" ]; then
     java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A
-elif [ "$2" <> "0.51" ]; then
+elif [ "$1" = "true" ] && [ "$2" <> "0.51" ]; then
     java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A -ot $2
 else
     java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
