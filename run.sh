0ff745f05287e4f3f47a96fa35ee1c6c44bed3d6
Sky Blaise <69448615+SkyBlaise99@users.noreply.github.com>
2024-04-03 14:15:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A
+if [ "$1" = "true" ]; then
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date -A
+else
+    java -jar RepoSense.jar --config ./configs --until 21/04/2023 --since 17/02/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+fi
